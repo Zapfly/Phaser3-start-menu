df6a0d504b08109beebffa692d9bd0eee5810651
Zapfly <seegerlucas295@gmail.com>
2022-12-12 19:17:19 -0700

    added start button (no function)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import logoImg from './assets/logo.png';
 import background from './assets/background-castle.jpg'
 import TitleScene from './scenes/TitleScene'
 
+function loadFont(name, url) {
+    var newFont = new FontFace(name, `url(${url})`);
+    newFont.load().then(function (loaded) {
+        document.fonts.add(loaded);
+    }).catch(function (error) {
+        return error;
+    });
+}
+
 class MyGame extends Phaser.Scene
 {
     constructor ()
@@ -14,6 +23,8 @@ class MyGame extends Phaser.Scene
     {
         this.load.image('logo', logoImg);
         this.load.image('background', background)
+        // loadFont('MontserratVariableFont', './fonts/Montserrat-VariableFont_wght.ttf')
+        // loadFont('Montserrat-ExtraBold', './fonts/Montserrat-ExtraBold.ttf')
     }
       
     create ()
@@ -26,7 +37,7 @@ class MyGame extends Phaser.Scene
 
 
         const logo = this.add.image(400, 300, 'logo');
-        let creditStatement = this.add.text(400, 100, 'POWERED BY').setFont('50px Ariel')
+        let creditStatement = this.add.text(400, 100, 'POWERED BY', { fontFamily: 'Montserrat-ExtraBold'})
         creditStatement.setOrigin(0.5, 0.5)
         creditStatement.setFontSize = 200;
       
@@ -42,6 +53,8 @@ class MyGame extends Phaser.Scene
         this.input.once('pointerdown', function () {
             game.scene.start('TitleScene')
         })
+
+        // let menuButton = this.add.dom(200, 200, 'div', 'background-color: green;')
     }
 
 
@@ -54,12 +67,15 @@ const config = {
     parent: 'phaser-example',
     width: 800,
     height: 600,
+    dom: {
+        createContainer: true
+    },
     // scene: [MyGame, TitleScene]
 };
 
 const game = new Phaser.Game(config);
 game.scene.add('MyGame', MyGame);
-game.scene.start('MyGame')
+// game.scene.start('MyGame')
 
 game.scene.add('TitleScene', TitleScene);
-// game.scene.start('TitleScene')
+game.scene.start('TitleScene')
--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -1,4 +1,6 @@
 import background from '../assets/background-castle.jpg'
+// import menuButtons from '../assets/grey buttons.png'
+import startButton from '../assets/start-button.png'
 
 class TitleScene extends Phaser.Scene{
     constructor() {
@@ -8,13 +10,35 @@ class TitleScene extends Phaser.Scene{
     preload() {
         this.load.image('background_image',);
         this.load.image('background-castle', background);
+        this.load.image('start button', startButton)
 
+        // this.load.spriteSheet('buttons', '../assets/grey-buttons.png', { frameWidth: 158, frameHeight: 52})
     }
     create() {
-        const background = this.add.image(300, 300, 'background-castle')
+
+        // let buttonStyle = {
+        //     'width' : 'fit-content',
+        //     'padding' : '10px'
+        // }
+
+
+        // let startButton = document.createElement(300, 300, 'div', null, 'Start');
+
+
+        
+        const background = this.add.image(300, 300, 'background-castle');
         let backgroundScaleFactor = 600/1080;
         background.scaleX = backgroundScaleFactor;
         background.scaleY = backgroundScaleFactor;
+        
+
+        // let menuButton = this.add.rectangle(400, 300, 100, 40, 0xa2a2a2)
+        let buttons = this.add.group();
+
+        let startMenuButton = buttons.create(400, 100, 'start button').setInteractive()
+        
+
+        
     }
 }
 
